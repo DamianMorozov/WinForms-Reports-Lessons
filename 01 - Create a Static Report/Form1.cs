077be8845119099e77eaa7eee8f9c07cbe9f9ff9
Damian <web@morozov33.ru>
2017-12-06 13:28:26 +0300

    1.0.1 Первая сборка

---
+++ 01 - Create a Static Report/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Create_Static_Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
